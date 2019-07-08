5a17d0fec602cdbcf184ce1240ebcaad84b691c3
Donovan Lambert <donovan@insolus.com>
2019-07-08 16:45:19 +0200

    Cleaning

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import replace from 'rollup-plugin-replace'
-import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
-import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 const input = './src/index.js'
@@ -37,34 +35,13 @@ export default [
       babel(babelOptions),
       commonjs(commonjsOptions),
       replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
-      sizeSnapshot(),
     ],
   },
-
-  {
-    input,
-    output: {
-      file: './dist/react-big-calendar.min.js',
-      format: 'umd',
-      name,
-      globals,
-    },
-    external: Object.keys(globals),
-    plugins: [
-      nodeResolve(),
-      babel(babelOptions),
-      commonjs(commonjsOptions),
-      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-      sizeSnapshot(),
-      terser(),
-    ],
-  },
-
   {
     input,
     output: { file: pkg.module, format: 'esm' },
     // prevent bundling all dependencies
     external: id => !id.startsWith('.') && !id.startsWith('/'),
-    plugins: [babel(babelOptions), sizeSnapshot()],
+    plugins: [babel(babelOptions)],
   },
 ]
